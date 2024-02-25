a99f21015664fb5d7f06b75ba6ff9ec0e78268bf
Hemika Roy <hemika44@gmail.com>
2024-02-25 17:06:44 -0500

    add filter dropdown and new task button UI

--- app/src/main/java/com/example/famplanapp/Tasks.kt
+++ app/src/main/java/com/example/famplanapp/Tasks.kt
@@ -1,12 +1,129 @@
 package com.example.famplanapp
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.DropdownMenu
+import androidx.compose.material.DropdownMenuItem
+import androidx.compose.material.Icon
+import androidx.compose.material.OutlinedButton
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.material3.OutlinedIconButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.toSize
 
 @Composable
-fun Tasks(innerPadding: PaddingValues){
-    Text("Tasks", Modifier.padding(innerPadding))
+fun Tasks(innerPadding: PaddingValues) {
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Column(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            // Dropdown menu in the top left
+            FilterDropdown()
+            // insert list in middle here
+        }
+
+        // Button in the bottom right
+        Box(
+            modifier = Modifier
+                .align(Alignment.BottomEnd)
+                .padding(bottom = 60.dp, end = 16.dp)
+        ) {
+            NewTaskButton()
+        }
+    }
+}
+
+@Composable
+fun NewTaskButton() {
+   OutlinedButton(
+       onClick = { /*TODO*/ }
+   ) {
+       Spacer(modifier = Modifier.size(ButtonDefaults.IconSpacing))
+       Text("New Task")
+   }
+}
+
+@Composable
+fun FilterDropdown() {
+    var expanded by remember { mutableStateOf(false) }
+    val options = listOf("My Tasks", "All Tasks", "Subscribed Tasks", "Unassigned Tasks")
+    var selectedItem by remember { mutableStateOf("My Tasks") }
+    //var mTextFieldSize by remember { mutableStateOf(Size.Zero)}
+
+    val icon = if (expanded)
+        Icons.Filled.KeyboardArrowUp
+    else
+        Icons.Filled.KeyboardArrowDown
+
+    Column(Modifier.padding(20.dp)) {
+        OutlinedTextField(
+            value = selectedItem,
+            onValueChange = { selectedItem = it },
+            modifier = Modifier
+                .fillMaxWidth(0.5f)
+                .onGloballyPositioned { coordinates ->
+                },
+            trailingIcon = {
+                Icon(icon,"contentDescription",
+                    Modifier.clickable { expanded = !expanded })
+            },
+            readOnly = true,
+            keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.None)
+        )
+
+        DropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false },
+            modifier = Modifier
+                .fillMaxWidth(0.5f)
+        ) {
+            options.forEach { label ->
+                DropdownMenuItem(onClick = {
+                    selectedItem = label
+                    expanded = false
+                }) {
+                    Text(text = label)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun NewTaskScreen() {
+
+
 }
