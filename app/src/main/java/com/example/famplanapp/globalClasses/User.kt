cfb9ced93951a5c770c914bf3126d2a4752953bf
Hemika Roy <hemika44@gmail.com>
2024-03-26 17:33:08 -0400

    user class's tasks list now mutable

--- app/src/main/java/com/example/famplanapp/globalClasses/User.kt
+++ app/src/main/java/com/example/famplanapp/globalClasses/User.kt
@@ -6,7 +6,7 @@ class User(
     var name: String = "",
     var preferredName: String = "",
     var email: String = "",
-    var tasks: List<Task> = mutableListOf<Task>(),
+    var tasks: MutableList<Task> = mutableListOf<Task>(),
     var colour: String = "",
     var role: String = "",
     settings: AppSettings = AppSettings()) {
